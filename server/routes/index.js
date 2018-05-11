b2544c22f3a6bcf6b2f23765fc9c6f88494968b2
Yuttasak Pannawat <yuttasakcom@gmail.com>
2018-05-11 13:39:59 +0700

    add logger middleware to api endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const logger = require("morgan");
 
 const api = require("./api");
-router.use("/api", api);
+router.use("/api", logger("common"), api);
 
 module.exports = router;
